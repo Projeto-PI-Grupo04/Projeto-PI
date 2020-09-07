8392ff2b77c513d32d64da722160ccb917c4ed45
AlexandreSGJR <alexandressgjr@gmail.com>
2020-09-07 12:17:56 -0300

    Criação do banco_v2

--- projeto_pi.sql
+++ projeto_pi.sql
@@ -1,24 +1,26 @@
 create database BancoDados;
 use BancoDados;
 create table Temperatura (
-idTemp int primary key,
-Graus decimal,
-horario time,
-Dia date
+	idTemp int primary key auto_increment,
+	Graus decimal,
+	horario time,
+	Dia date
 );
 create table Umidade (
-idUmidade int primary key auto_increment,
-umidade decimal
+	idUmidade int primary key auto_increment,
+	umidade decimal
 );
-create table UsuarioInfo (
-id int primary key,
-Nome varchar(40),
-CPF char(11),
-telefone int
+create table tbUsuarioInfo (
+	idUsuarioInfo int primary key auto_increment,
+	Nome varchar(40),
+	CPF char(11),
+	telefone int
 );
-create table Usuario (
-id int primary key,
-login varchar(30),
-senha varchar(30),
-check (Nível_de_acesso = '1' or Nível_de_acesso ='2')
+create table loginUsuario (
+	idUsuario int primary key,
+	login varchar(30),
+	senha varchar(30),
+	check (Nível_de_acesso = '1' or Nível_de_acesso ='2'),
+    fk_idUsuarioInfo int,
+		foreign key (fk_idUsuarioInfo) references tbUsuarioInfo (idUsuarioInfo)
 );
