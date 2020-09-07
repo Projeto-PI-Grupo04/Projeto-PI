c48cec7f17dd83c4155019af35179b7c35e1530e
AlexandreSGJR <alexandressgjr@gmail.com>
2020-09-07 14:14:57 -0300

    Atualização do BD

--- BD/projeto_pi.sql
+++ BD/projeto_pi.sql
@@ -1,29 +1,65 @@
 create database BancoDados;
 use BancoDados;
-create table Temperatura (
-	idTemp int primary key auto_increment,
+
+create table tbUsuario (
+	idUsuario int primary key auto_increment,
+	nomeUsuario varchar(40),
+	cpfUsuario char(11)
+);
+
+create table tbFoneUsuario (
+	idFoneUsuario int primary key auto_increment,
+    numFoneUsuario int,
+    fk_idUsuario int,
+    foreign key (fk_idUsuario) references tbUsuario (idUsuario)
+);
+
+create table tbLoginUsuario (
+	idUsuario int primary key,
+	emailUsuario varchar(30),
+	senhaUsuario varchar(30),
+    nivelAcesso int,
+	check (nivelAcesso = '1' or nivelAcesso ='2'),
+    fk_idUsuario int,
+		foreign key (fk_idUsuario) references tbUsuario (idUsuario)
+);
+
+create table tbTemperatura (
+	idTemperatura int primary key auto_increment,
 	Graus decimal,
 	horario time,
 	Dia date
 );
-create table Umidade (
+
+create table tbUmidade (
 	idUmidade int primary key auto_increment,
-	umidade decimal
-);
-create table tbUsuarioInfo (
-	idUsuarioInfo int primary key auto_increment,
-	Nome varchar(40),
-	CPF char(11),
-	telefone int
+	nivelUmidade decimal,
+    horario time,
+    dia date
 );
 
--- blablabla --
-create table loginUsuario (
-	idUsuario int primary key,
-	login varchar(30),
-	senha varchar(30),
-    Nivel_de_acesso int,
-	check (Nivel_de_acesso = '1' or Nivel_de_acesso ='2'),
-    fk_idUsuarioInfo int,
-		foreign key (fk_idUsuarioInfo) references tbUsuarioInfo (idUsuarioInfo)
+create table tbSensor (
+	idSensor int primary key auto_increment,
+    nomeSensor varchar(50),
+    statusSensor varchar(50),
+    tipoSensor varchar(50)
 );
+
+
+create table tbTemperaturaSensor (
+	idTemperaturaSensor int primary key auto_increment,
+    fk_idSensor int,
+		foreign key (fk_idSensor) references tbSensor (idSensor),
+	fk_idTemperatura int,
+		foreign key (fk_idTemperatura) references tbTemperatura (idTemperatura)
+);
+
+create table tbUmidadeSensor (
+	idUmidadeSensor int primary key auto_increment,
+    fk_idSensor int,
+		foreign key (fk_idSensor) references tbSensor (idSensor),
+    fk_idUmidadeSensor int,
+		foreign key (fk_idUmidadeSensor) references tbSensor (idSensor)
+);
+
+drop database bancodados;
